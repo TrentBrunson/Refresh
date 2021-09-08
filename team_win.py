cca99eaf7dcee69ab77616747a0747fb74652e79
TrentBrunson <bbiz2@icloud.com>
2021-09-07 20:21:38 -0500

    team winning percentage complete

--- team_win.py
+++ team_win.py
@@ -1,5 +1,11 @@
 # User input favorite team, games won, and number of games lost last season. 
 # Display team name and the percentage of games won. 
 
+teamName = input("What is the name of your favorite team? ")
+print()
+teamWins = int(input("How many games have they won? "))
+print()
+teamLosses = int(input("How many games have the lost? "))
+print()
 
-
+print(f"The {teamName} had a winning percentage of {(teamWins / (teamWins + teamLosses)) * 100:.1f}%.")
--- world_pop_growth.py
+++ world_pop_growth.py
@@ -1,5 +1,6 @@
-# The world population reached 7 billion people on October 21, 2011. Assuming the world population continues to grow at 1.1% annually, 
-# Provide population forecast
+# The world population reached 7 billion people on October 21, 2011. 
+# Assuming the world population continues to grow at 1.1% annually, 
+# provide the population forecast
 
 print(
     f"************************************************\n"
@@ -7,12 +8,12 @@ print(
     f"************************************************\n\n"
 )
 
-newWorldPop, worldPop = 7000000000
+newWorldPop = worldPop = 7000000000
 year = 2011
 
 while newWorldPop < 8000000000:
-    newWorldPop = newWorldPop * 1.1
+    newWorldPop = newWorldPop * 1.01
     year += 1
-    print(f"The population in {year} is {newWorldPop:.0f}")
+    print(f"The population in {year} is {newWorldPop:,.0f}")
 
-print(f"The world's population will exceed 8,000,000,000 in {year}")
+print(f"At a 1.1% annual growth rate, the world's population will exceed 8,000,000,000 in {year}.")
