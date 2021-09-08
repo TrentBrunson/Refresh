aae4a417cfbb0dd9a6b3d100ea55539cb0c4762f
TrentBrunson <bbiz2@icloud.com>
2021-09-07 20:36:01 -0500

    change maker complete

--- change_maker.py
+++ change_maker.py
@@ -1,2 +1,25 @@
 # Aa program to make change for an amount of money from 0 through 99 cents input by the user. 
 # Show the number of coins from each denomination (i.e., quarters, dimes, nickels, and pennies) used to make the change. 
+
+print(
+    f"****************************************\n"
+    f"This program calculates how make change.\n"
+    f"****************************************\n"
+)
+
+change = int(input("Enter how much change you need to make in coins (0-99): "))
+print()
+
+quarters = change // 25
+dimes = (change - (quarters * 25)) // 10
+nickels = (change - (quarters * 25) - (dimes * 10)) // 5
+pennies = change - (quarters * 25) - (dimes * 10) - (nickels * 5)
+
+print(
+    f"To make your change you need:\n"
+    f"Quarters: {quarters}\n"
+    f"Dimes:    {dimes}\n"
+    f"Nickles:  {nickels}\n"
+    f"Pennies:  {pennies}\n"
+)
+ 
