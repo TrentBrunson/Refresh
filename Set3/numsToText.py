cd22a7e1e757aff640ae83cd3e0be6c4c4f89f9b
TrentBrunson <bbiz2@icloud.com>
2021-09-17 20:47:19 -0500

    number to text

--- Set3/numsToText.py
+++ Set3/numsToText.py
@@ -2,11 +2,59 @@
 
 # AUTHOR:     Trent Brunson
 # COURSE:     ANLY 615
-# PROGRAM:    Letter e counter
+# PROGRAM:    Reading Numbers
 # PURPOSE:    Count the letter e in a string.
 # INPUT:      user string of characters
 # PROCESS:    treat the input string as a list of characters
 # OUTPUT:     number of e's in the input
 # HONOR CODE: On my honor, as an Aggie, I have neither given 
 #             nor received unauthorized aid on this academic work.
-#%%
+# %%
+"""
+Pseudocode
+Write a program that asks the user to enter a number with 27 digits or less. 
+The convert that number into a set of three-digit numbers and associated 
+place values. Display the results to the user.
+
+1. get input and convert to string
+2. get length of string
+3. loop through number string
+    a. how many 3-digit segments are ther? 
+    b. howbigofaloop = n/3
+    c. if condition:
+        elif:
+        elif:
+        else:
+    d. edge case: 0?
+        if num == 0
+            return "zero"
+        while foo > 0:
+            if num % 1000 != 0
+if some length > 24
+    septillion = True
+
+            i += 1
+"""
+
+choice = "Y"
+while choice == "Y":
+
+    if number == 0
+        return "Zero"
+    elif:
+    number = int(input("Enter your number: "))
+    numberStr = str(number)
+    print(len(numberStr))
+    
+    units = [" ", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion"]
+
+    # numberslicer = numberStr[-3:]
+    # print(numberslicer)
+    numberslicer = numberStr[-3:]
+    print(numberslicer)
+
+    choice = input("Would you like to enter another number? (Y/N)")
+
+
+
+# %%
