8d1cb058ae2156408130e28ff9a3b31896ca1e98
TrentBrunson <bbiz2@icloud.com>
2021-09-19 15:52:44 -0500

    palidrome complete

--- Set3/palindrome.py
+++ Set3/palindrome.py
@@ -29,12 +29,9 @@ print("This program determines if your inputs are plaindromes.")
 
 s1 = str(input("Enter your first word or phrase: "))
 s2 = str(input("Enter your second word or phrase: "))
-# Replace special characters with ''
-s1Stripped = list(''.join(ch for ch in s1 if ch.isalnum()))
-# s2Stripped = list(''.join(ch for ch in s2 if ch.isalnum()))
-s2RevStripped = list(reversed(''.join(ch for ch in s2 if ch.isalnum())))
-
-# s2RevStripped = list(reversed(s2Stripped))
+# Replace special characters with '' and reverse second string for compare
+s1Stripped = list(''.join(ch for ch in s1 if ch.isalnum()).lower())
+s2RevStripped = list(reversed(''.join(ch for ch in s2 if ch.isalnum()).lower()))
 
 if s1Stripped == s2RevStripped:
     print(
@@ -42,4 +39,40 @@ if s1Stripped == s2RevStripped:
     )
 else: print(f"You do not have a palindrome.")
 print(s2RevStripped)
+# %%
+def str_rev(string):
+    return string[::-1]
+
+def get_words():
+    s1 = str(input("Enter your first word or phrase: "))
+    s2 = str(input("Enter your second word or phrase: "))
+    return s1, s2
+
+def alpha_num_word(a1, a2):
+    # get only alphanumerics and convert to lower case for comparison
+    # reverse second string for comparison
+    s1Stripped = list(''.join(ch for ch in a1 if ch.isalnum()).lower())
+    s2Stripped = list(''.join(ch for ch in a2 if ch.isalnum()).lower())
+    s2Rev = str_rev(s2Stripped)
+    return s1Stripped, s2Rev
+
+def palindrome(p1, p2, s1, s2):
+    if p1 == p2:
+        print(
+            f"Your words {s1} and {s2} are a palindrome."
+        )
+    else: print(f"You do not have a palindrome.")
+    return 
+
+def main():
+    choice = "Y"
+    while choice.upper() == "Y":
+        print("This program determines if your inputs are plaindromes.")
+        user1, user2 = get_words()
+        noSpecial1, noSpecial2 = alpha_num_word(user1, user2)
+        palindrome(noSpecial1, noSpecial2, user1, user2)
+        choice = input("Would you like to try another set or phrase? (Y/N)")
+
+if __name__ == "__main__":
+    main()
 # %%
