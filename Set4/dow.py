e90ad94a781cf3235edfd622d76d1cafd72ce5c0
TrentBrunson <bbiz2@icloud.com>
2021-09-19 16:02:01 -0500

    DOW 30

---
+++ Set4/dow.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# AUTHOR:     Trent Brunson
+# COURSE:     ANLY 615
+# PROGRAM:    Palindrome
+# PURPOSE:    Identify if the two inputs meet the criteria of a palindrome.
+# INPUT:      User input two strings of words or phrases.
+# PROCESS:    Strip puntucation and spaces.  Reverse order of second input and compare.
+# OUTPUT:     If true, display acknowledgement that inputs are a palindrome, otherwise, no.
+# HONOR CODE: On my honor, as an Aggie, I have neither given 
+#             nor received unauthorized aid on this academic work.
+
+"""
+Write a program that displays the stock symbol for all 30 companies in a 3 X 10 table. 
+Ask the user to enter a symbol and then display the details for that company. 
+The sample output for one user session is as follows:
+
+Symbols for the Thirty DOW Stocks
+AXP      BA       CAT      CSCO     CVX      DD       DIS      GE       GS       HD
+IBM      INTC     JNJ      JPM      KO       MCD      MMM      MRK      MSFT     NKE
+PFE      PG       T        TRV      UNH      UTX      V        VZ       WMT      XOM      
+
+Enter a symbol: MCD
+Company: McDonald's
+Industry: Fast food
+Exchange: NYSE
+Growth in 2013: 10.00%
+Price/Earnings ratio in 2013: 17.48
+"""
+#%%
+# iterate through rows of txt file and print to screen
+
+tickerList = []
+
+with open("data/dow.txt") as file:
+    for line in file:
+        ticker = line.strip()
+        tickerList.append(ticker)
+
+print(tickerList)
+# %%
