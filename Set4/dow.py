32dadc83155ffb761899c06cda1055901a0216ae
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2021-09-22 13:47:21 -0500

    DOW functions tested and working

--- Set4/dow.py
+++ Set4/dow.py
@@ -31,7 +31,6 @@ Price/Earnings ratio in 2013: 17.48
 """
 #%%
 # iterate through rows of txt file and print to screen
-
 tickerList = []
 tickerSymbol = []
 DOWdict = {}
@@ -50,6 +49,7 @@ while choice.upper() == "Y":
     print(*tickerSymbol[:10], sep="\t")
     print(*tickerSymbol[10:20], sep="\t")
     print(*tickerSymbol[-10:], sep="\t")
+    print("To exit program, enter: EXIT")
     print()
 
     DOWdict = dict(zip(tickerSymbol, tickerList))
@@ -74,4 +74,62 @@ while choice.upper() == "Y":
         print("Only enter ticker symbols from the menu.\n\n")
         continue
     choice = input("Would you like to make another selection? (Y/N) ")
+# %%
+# iterate through rows of txt file and print to screen
+
+tickerList = []
+tickerSymbol = []
+DOWdict = {}
+choice = "Y"
+
+def get_data_to_list():
+    with open("data/dow.txt") as file:
+        for line in file:
+            currentLine = line.split(',')
+            tickerList.append(currentLine)
+    for i in range(30):
+        tickerSymbol.append(tickerList[i][1]) 
+    return tickerList, tickerSymbol
+
+def menu(tickers):
+    print("Symbols for the Thirty DOW Stocks")
+    # print on 10 symbols per row on 3 rows
+    print(*tickers[:10], sep="\t")
+    print(*tickers[10:20], sep="\t")
+    print(*tickers[-10:], sep="\t")
+    print("To exit program, enter: EXIT")
+    print()
+
+def dow_calc(tickerList, tickerSymbol):
+    choice = "Y"
+    DOWdict = dict(zip(tickerSymbol, tickerList))
+    while choice.upper() == "Y":
+        try:
+            company = str(input("Enter a symbol: ").upper())
+        except ValueError:
+            print("Only enter ticker symbols.\n\n")
+            continue
+        if company.upper() == "EXIT":
+            break
+        elif company in tickerSymbol:
+            print(
+                f"Enter a symbol: {company}\n"
+                f"Company: {DOWdict[company][0]}\n"
+                f"Industry: {DOWdict[company][3]}\n"
+                f"Exhange: {DOWdict[company][2]}\n"
+                f"Growth in 2013: {((float(DOWdict[company][5])/float(DOWdict[company][4]))-1) * 100:.2f}%\n"
+                f"Price/Earnings ratio in 2013: {float(DOWdict[company][5])/float(DOWdict[company][6]):.2f}\n"
+            )
+        else:
+            print("Only enter ticker symbols from the menu.\n\n")
+            continue
+        choice = input("Would you like to make another selection? (Y/N) ")
+
+def main():
+    dowList, ticker = get_data_to_list()
+    menu(ticker)
+    dow_calc(dowList, ticker)
+
+if __name__ == "__main__":
+    main()
 # %%
