abc67652dda8fc94ee9c208d33b9b1e513ad634a
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2021-09-22 21:02:20 -0500

    physics files formatted

--- Set4/physics.py
+++ Set4/physics.py
@@ -28,12 +28,9 @@ velocity = float(input("Enter the initial velocity (feet/second): "))
 timeToMaxHeight = velocity/32
 totalDistance = t = 0
 table = []
-# {"Time":[], "height":[]}
-# total time flown also equal 2 *  height
 
 for t in range(0, int(round(timeToMaxHeight + 1))):
     hOFt = height + velocity * t - 16 * t ** 2
-    # totalDistance += hOFt
     # list of lists with time as the key for the output variables
     table.append([t, hOFt, hOFt])
     t += 1
@@ -42,7 +39,7 @@ apex = apexHeight = hOFt + height
 timeToFall = (2 * apex / 32) ** 0.5 # SQRT(ft / (ft/sec^2))
 impactVelocity = 32 * timeToFall # (ft/sec^2 *  sec)
 
-print(*table, "\n\n", "apex: ", apex, " --time to fall: ", timeToFall, " --impact velocity", impactVelocity, "\n")
+# print(*table, "\n\n", "apex: ", apex, " --time to fall: ", timeToFall, " --impact velocity", impactVelocity, "\n")
 
 descentTable = []
 # use list comprehension to step through in increments of 0.1 
@@ -58,9 +55,9 @@ for time in [t/10 for t in range(0, int(round(timeToFall*10)))]:
         break
     time += 0.1
 
-print(*descentTable)
-print("\n\n")
-print(*table)
+# print(*descentTable)
+# print("\n\n")
+# print(*table)
 """
 Output table:
 Time in seconds, height and distance traveled
@@ -108,9 +105,9 @@ def chicken_up(startH, startV):
         tableUp.append([t, hOFt, hOFt])
         t += 1
     apex = hOFt + startH
-    return apex, tableUp
+    return apex, tableUp, t
 
-def chicken_down(crest, finaltable, ttf):
+def chicken_down(crest, finaltable, timeUp, ttf):
     """
     Time, distance and height for a spherical chicken falling in a vacuum.
     """
@@ -119,23 +116,21 @@ def chicken_down(crest, finaltable, ttf):
     # use list comprehension to step through in increments of 0.1 
     # or increase everything by 10 in the range and divide the functions by 10
     for time in [t/10 for t in range(0, int(round(ttf*10)))]:
-        velocity = time * 32
-        heightDelta = velocity * time
+        heightDelta = (time ** 2) *32
         descHeight = crest-(heightDelta)
-        tableDown.append([round(time,1), velocity, descHeight])
+        tableDown.append([round(time,1), heightDelta, descHeight])
         # only add whole seconds to table as to not have different units of output
         if time.is_integer(): 
-            finaltable.append([time+t, round(descHeight,2), round(crest + heightDelta,2)])
+            finaltable.append([time+timeUp, round(descHeight,2), round(crest + heightDelta,2)])
         if descHeight < 0:
             break
         time += 0.1
-    # print(*tableDown)
     return finaltable, time
 
 def outputs(table, height, velocity, apex, time, fallTime):
     print(f"{'Time':<10}{'Actual Height':<20}{'Total Distance':<20}")
     for i in range(len(table)):
-        print(f"{int(table[i][0]):<10}{table[i][1]:<20}{table[i][2]:<20}")
+        print(f"{round(table[i][0],1):<10}{table[i][1]:<20}{table[i][2]:<20}")
     print(f"Your starting parameters:\nHeight: {height} feet\nStarting velocity: {velocity} ft/sec"
         f"\n\nTotal time: {round(time + fallTime,3)} seconds, Apex: {apex} feet, "
         f"Total distance traveled: {2*apex} feet"
@@ -144,24 +139,19 @@ def outputs(table, height, velocity, apex, time, fallTime):
     with open("object-trajectory.txt", "w") as f:
         f.writelines(f"Your starting parameters:\nHeight: {height} feet\nStarting velocity: {velocity} ft/sec\n")
         f.writelines(f"{'Time':<15}{'Actual Height':<15}{'Total Distance':<15}\n")
-        # for line in table:
         for row in table:
             for field in row:
-                f.writelines(f"{field:<15}")
+                f.writelines(f"{round(field,1):<15}")
             f.writelines("\n")        
         f.write(f"Total time: {round(time + fallTime,3)} seconds, Apex: {apex} feet, "
             f"Total distance traveled: {2*apex} feet"
             )
-    with open("o2.txt", "w") as newF:
-        for row in table:
-            for field in row:
-                newF.writelines(f"{field},")
-            newF.writelines("\n")
+
 def main():
     ht, vel = get_params()
-    peak, upTable = chicken_up(ht, vel)
+    peak, upTable, upTime = chicken_up(ht, vel)
     tof, impactV = physics_check(peak)
-    fTable, time = chicken_down(peak, upTable, tof)
+    fTable, time = chicken_down(peak, upTable, tof, upTime)
     outputs(fTable, ht, vel, peak, time, tof)
 
 if __name__ == "__main__":
